b7b215bcc07956be8c6d8c9642cc9711694fd177
WASDetchan <yarik.antipov2008@gmail.com>
2025-08-14 15:46:05 +0300

    Allocate and begin command buffer

--- src/vk/command_buffer.rs
+++ src/vk/command_buffer.rs
@@ -1,1 +1,57 @@
+use std::sync::{Arc, Weak};
 
+use ash::vk;
+
+use super::{command_pool::CommandPool, device::Device, error::fatal_vk_error};
+
+#[derive(Debug, strum::Display, Clone, Copy)]
+pub enum CommandBufferState {
+    Initial,
+    Recording,
+    Executable,
+    Pending,
+    Invalid,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Invalid command buffer state: {0}")]
+pub struct CommandBufferStateError(pub CommandBufferState);
+
+pub struct CommandBuffer {
+    command_pool: Arc<CommandPool>,
+    device: Arc<Device>,
+    command_buffer: vk::CommandBuffer,
+    state: CommandBufferState,
+}
+
+impl CommandBuffer {
+    pub fn new(
+        command_pool: Arc<CommandPool>,
+        device: Arc<Device>,
+        command_buffer: vk::CommandBuffer,
+    ) -> Self {
+        CommandBuffer {
+            command_pool,
+            device,
+            command_buffer,
+            state: CommandBufferState::Initial,
+        }
+    }
+
+    pub fn begin(&mut self) -> Result<(), CommandBufferStateError> {
+        match self.state {
+            CommandBufferState::Initial => (),
+            CommandBufferState::Executable => (),
+            state => return Err(CommandBufferStateError(state)),
+        };
+        let begin_info = vk::CommandBufferBeginInfo::default();
+        unsafe {
+            self.device
+                .raw_handle()
+                .begin_command_buffer(self.command_buffer, &begin_info)
+                .unwrap_or_else(|error| fatal_vk_error("failed to begin_command_buffer", error))
+        }
+        self.state = CommandBufferState::Recording;
+        Ok(())
+    }
+}
--- src/vk/command_pool.rs
+++ src/vk/command_pool.rs
@@ -1,8 +1,8 @@
-use std::sync::Arc;
+use std::sync::{Arc, Weak};
 
 use ash::vk;
 
-use super::{device::Device, error::fatal_vk_error};
+use super::{command_buffer::CommandBuffer, device::Device, error::fatal_vk_error};
 
 #[derive(Debug, thiserror::Error)]
 pub enum CommandPoolCreationError {
@@ -11,6 +11,7 @@ pub enum CommandPoolCreationError {
 }
 
 pub struct CommandPool {
+    weak_self: Weak<Self>,
     device: Arc<Device>,
     command_pool: vk::CommandPool,
 }
@@ -19,7 +20,7 @@ impl CommandPool {
     pub fn new(
         device: Arc<Device>,
         queue_family_index: u32,
-    ) -> Result<Self, CommandPoolCreationError> {
+    ) -> Result<Arc<Self>, CommandPoolCreationError> {
         if queue_family_index as usize >= device.get_queue_family_count() {
             return Err(CommandPoolCreationError::InvalidQueueFamily(
                 queue_family_index as usize,
@@ -31,10 +32,31 @@ impl CommandPool {
         let command_pool = unsafe { device.raw_handle().create_command_pool(&create_info, None) }
             .unwrap_or_else(|error| fatal_vk_error("failed to create_command_pool", error));
 
-        Ok(Self {
+        Ok(Arc::new_cyclic(|weak_self| Self {
+            weak_self: Weak::clone(weak_self),
             device,
             command_pool,
-        })
+        }))
+    }
+
+    pub fn allocate_command_buffer(&self) -> CommandBuffer {
+        let allocate_info = vk::CommandBufferAllocateInfo::default()
+            .command_pool(self.command_pool)
+            .command_buffer_count(1)
+            .level(vk::CommandBufferLevel::PRIMARY);
+
+        let command_buffer = unsafe {
+            self.device
+                .raw_handle()
+                .allocate_command_buffers(&allocate_info)
+                .unwrap_or_else(|error| fatal_vk_error("failed to allocate_command_buffer", error))
+                [0]
+        };
+        CommandBuffer::new(
+            self.weak_self.upgrade().unwrap(),
+            self.device.clone(),
+            command_buffer,
+        )
     }
 }
 
